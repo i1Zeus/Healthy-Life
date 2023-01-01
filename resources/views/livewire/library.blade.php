09cdbaf62d131a771fa94101213a1981ef22401c
Hussein Najah <husseinnajah123@gmail.com>
2023-01-01 16:59:18 +0300

    s

---
+++ resources/views/livewire/library.blade.php
@@ -0,0 +1,11 @@
+<div>
+    <div class="text-6xl bg-black">
+        <p class="text-stone-50">
+            Body care is the practice of caring for the skin, hair, nails, and body. It is a form of self-care. Body
+            care is
+            often done in the morning and evening. It is important to take care of the body to keep it healthy and
+            clean.
+        </p>
+    </div>
+
+</div>
