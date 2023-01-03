edd480e84cad8984a3a67bcffcb2cccd72f5c5e4
zal3 <102377778+zal3@users.noreply.github.com>
2023-01-03 23:40:51 +0300

    a

--- app/Http/Livewire/HealthForm.php
+++ app/Http/Livewire/HealthForm.php
@@ -6,6 +6,15 @@ use Livewire\Component;
 
 class HealthForm extends Component
 {
+    public $hight , $weight , $age , $gender  , $result;
+    public function harrisbenedict()
+        {
+            // if($this->gender == 1) //for man 
+                $this->result = 66 + (6.2 * $this->weight) + (12.7 * $this->hight) - (6.76 * $this->age);
+            // else //for woman
+                // $this->result =655 + (4.35* $this->weight) + (4.7 * $this->hight) - (4.7 * $this->age);
+        }
+
     public function render()
     {
         return view('livewire.health-form');
