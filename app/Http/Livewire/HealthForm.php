1f5934270b124c7e9dbd134a15ed8e674d68c4a9
zal3 <102377778+zal3@users.noreply.github.com>
2023-01-03 23:56:00 +0300

    a

--- app/Http/Livewire/HealthForm.php
+++ app/Http/Livewire/HealthForm.php
@@ -9,14 +9,14 @@ class HealthForm extends Component
     public $hight , $weight , $age , $gender  , $result;
     public function harrisbenedict()
         {
-            // if($this->gender == 1) //for man 
+            if($this->gender == 1) //for man 
                 $this->result = 66 + (6.2 * $this->weight) + (12.7 * $this->hight) - (6.76 * $this->age);
-            // else //for woman
-                // $this->result =655 + (4.35* $this->weight) + (4.7 * $this->hight) - (4.7 * $this->age);
+            else //for woman
+                $this->result =655 + (4.35* $this->weight) + (4.7 * $this->hight) - (4.7 * $this->age);
         }
 
     public function render()
     {
-        return view('livewire.health-form');
+        return view('livewire.health-form' , ['result' => $this->result]);
     }
 }
--- resources/views/livewire/health-form.blade.php
+++ resources/views/livewire/health-form.blade.php
@@ -1,4 +1,4 @@
-<!-- <x-app-layout> -->
+
 <div>
     <div class="mt-[10%] ml-[30%] xl:ml-[20%]">
         <form class="w-[60%] xl:w-[80%]">
@@ -13,39 +13,38 @@
                 </div>
             </div> -->
             <div class="relative z-0 w-full mb-6 group">
-                <input wire:model.lazy="hight" type="number" name="floating_email" id="floating_email"
-                    class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-black dark:border-gray-600 dark:focus:border-green-600 focus:outline-none focus:ring-0 focus:border-green-600 peer"
-                    placeholder=" " required />
-                <label for="floating_email"
-                    class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-green-600 peer-focus:dark:text-greenborder-green-600 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Hight</label>
+                <input wire:model.lazy="hight" type="number" name="floating_email" id="floating_email" class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-black dark:border-gray-600 dark:focus:border-green-600 focus:outline-none focus:ring-0 focus:border-green-600 peer" placeholder=" " required />
+                <label for="floating_email" class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-green-600 peer-focus:dark:text-greenborder-green-600 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Hight</label>
             </div>
             <div class="relative z-0 w-full mb-6 group">
-                <input wire:model.lazy="weight" type="number" name="floating_weight" id="floating_weight"
-                    class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-black dark:border-gray-600 dark:focus:border-green-600 focus:outline-none focus:ring-0 focus:border-green-600 peer"
-                    placeholder=" " required />
-                <label for="floating_weight"
-                    class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-green-600 peer-focus:dark:text-greenborder-green-600 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Weight</label>
+                <input wire:model.lazy="weight" type="number" name="floating_weight" id="floating_weight" class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-black dark:border-gray-600 dark:focus:border-green-600 focus:outline-none focus:ring-0 focus:border-green-600 peer" placeholder=" " required />
+                <label for="floating_weight" class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-green-600 peer-focus:dark:text-greenborder-green-600 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Weight</label>
             </div>
             <div class="grid md:grid-cols-2 md:gap-6">
-                <div class="relative z-0 w-full mb-6 group">
-                    <input wire:model.lazy="gender" type="text" name="floating_first_name" id="floating_first_name"
-                        class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-black dark:border-gray-600 dark:focus:border-green-600 focus:outline-none focus:ring-0 focus:border-green-600 peer"
-                        placeholder=" " required />
-                    <label for="floating_first_name"
-                        class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-green-600 peer-focus:dark:text-greenborder-green-600 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Gender</label>
+                <div>
+                    <input type="radio" id="gender" name="male" wire:model.lazy="gender" value="1">
+                    <label for="male">Male</label>
+
+                    <input type="radio" id="gender" name="male" wire:model.lazy="gender" value="2">
+                    <label for="female">Female</label>
                 </div>
                 <div class="relative z-0 w-full mb-6 group">
-                    <input wire:model.lazy="age" type="number" name="floating_last_name" id="floating_last_name"
-                        class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-black dark:border-gray-600 dark:focus:border-green-600 focus:outline-none focus:ring-0 focus:border-green-600 peer"
-                        placeholder=" " required />
-                    <label for="floating_last_name"
-                        class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-green-600 peer-focus:dark:text-greenborder-green-600 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Age</label>
+                    <input wire:model.lazy="age" type="number" name="floating_last_name" id="floating_last_name" class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-black dark:border-gray-600 dark:focus:border-green-600 focus:outline-none focus:ring-0 focus:border-green-600 peer" placeholder=" " required />
+                    <label for="floating_last_name" class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-green-600 peer-focus:dark:text-greenborder-green-600 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Age</label>
                 </div>
             </div>
-            <button type="submit" wire:click="harrisbenedict"
-                class="text-white duration-150 bg-green-700 hover:bg-green-800 focus:ring-4 focus:outline-none focus:ring-green-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800">Submit</button>
-        {{}}
-            </form>
+            <button type="submit" wire:click="harrisbenedict" class="text-white duration-150 bg-green-700 hover:bg-green-800 focus:ring-4 focus:outline-none focus:ring-green-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800">Submit</button>
+        </form>
+        {{$result}}
     </div>
 </div>
-<!-- </x-app-layout> -->
+<!-- <script>
+     
+    hight = document.getElementById('hight');
+    weight = document.getElementById('weight');
+    age = document.getElementById('age');
+    gender = document.getElementById('gender');
+
+    function harrisbenedict() {
+        if(
+</script> -->
