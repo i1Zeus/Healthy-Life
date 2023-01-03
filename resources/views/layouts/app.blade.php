165d094b0e16db2cf29d7c092d82cd9ce075c4b4
Hussein Najah <husseinnajah123@gmail.com>
2023-01-03 22:52:48 +0300

    Update app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <!-- Fonts -->
     <link href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
-    <title>Health life</title>
+    <title>Healthy life</title>
     <!-- font link -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/11.min.css">
     <!-- css link -->
