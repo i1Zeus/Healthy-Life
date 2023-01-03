bb418a0e46cd036c35bc9b0e1d8555064e1978db
zal3 <102377778+zal3@users.noreply.github.com>
2023-01-03 22:51:51 +0300

    a

--- database/seeders/BookSeeder.php
+++ database/seeders/BookSeeder.php
@@ -17,38 +17,38 @@ class BookSeeder extends Seeder
     {
         //1
         Book::Create([
-            'name'=> 'Book Name 1',
-            'description' =>'Book Description 1',
-            'image_path'=> 'https://edit.org/photos/images/cat/book-covers-big-2019101610.jpg-1300.jpg',
-            'file_path'=> 'storage/pdf/book.pdf'
+            'name' => 'Book Name 1',
+            'description' => 'Book Description 1',
+            'image_path' => 'https://edit.org/photos/images/cat/book-covers-big-2019101610.jpg-1300.jpg',
+            'file_path' => 'storage/pdf/book.pdf'
         ]);
-        //1
+        //2
         Book::Create([
-            'name'=> 'Book Name 2',
-            'description' =>'Book Description 2',
-            'image_path'=> 'https://edit.org/photos/images/cat/book-covers-big-2019101610.jpg-1300.jpg',
-            'file_path'=> 'storage/pdf/book.pdf'                ]);
-        //1
+            'name' => 'Book Name 2',
+            'description' => 'Book Description 2',
+            'image_path' => 'https://edit.org/photos/images/cat/book-covers-big-2019101610.jpg-1300.jpg',
+            'file_path' => 'storage/pdf/book.pdf'
+        ]);
+        //3
         Book::Create([
-            'name'=> 'Book Name 3',
-            'description' =>'Book Description 3',
-            'image_path'=> 'https://edit.org/photos/images/cat/book-covers-big-2019101610.jpg-1300.jpg',
-            'file_path'=> 'storage/pdf/book.pdf'                ]);
-        //1
+            'name' => 'Book Name 3',
+            'description' => 'Book Description 3',
+            'image_path' => 'https://edit.org/photos/images/cat/book-covers-big-2019101610.jpg-1300.jpg',
+            'file_path' => 'storage/pdf/book.pdf'
+        ]);
+        //4
         Book::Create([
-            'name'=> 'Book Name 4',
-            'description' =>'Book Description 4',
-            'image_path'=> 'https://edit.org/photos/images/cat/book-covers-big-2019101610.jpg-1300.jpg',
-            'file_path'=> 'storage/pdf/book.pdf'                ]);
+            'name' => 'Book Name 4',
+            'description' => 'Book Description 4',
+            'image_path' => 'https://edit.org/photos/images/cat/book-covers-big-2019101610.jpg-1300.jpg',
+            'file_path' => 'storage/pdf/book.pdf'
+        ]);
+        //5
         Book::Create([
-            'name'=> 'Book Name 5',
-            'description' =>'Book Description 5',
-            'image_path'=> 'https://edit.org/photos/images/cat/book-covers-big-2019101610.jpg-1300.jpg',
-            'file_path'=> 'storage/pdf/book.pdf'                ]);
-        
-
-        
-
-
+            'name' => 'Book Name 5',
+            'description' => 'Book Description 5',
+            'image_path' => 'https://edit.org/photos/images/cat/book-covers-big-2019101610.jpg-1300.jpg',
+            'file_path' => 'storage/pdf/book.pdf'
+        ]);
     }
 }
