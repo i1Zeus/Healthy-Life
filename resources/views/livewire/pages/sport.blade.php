6e770e0bbbea1b4cbfbbaae5b89c561589fef54c
cssajad <122051948+cssajad@users.noreply.github.com>
2023-01-23 13:52:49 +0300

    sajad

--- resources/views/livewire/pages/sport.blade.php
+++ resources/views/livewire/pages/sport.blade.php
@@ -1,5 +1,174 @@
-<div>
-    {{-- In work, do what you enjoy. --}}
-    <h1 class="text-red-500 mt-10">Hello</h1>
-</div>
 
+    <div class="features">
+        <h3>Prefer 10 types of sports equipment and their benefits</h3>
+        <h6>There is no doubt that sports equipment has added a lot to our lives, such as better health, a fit body and a stronger heart, and other health benefits that the human body benefits from during exercise and psychological benefits that double the health benefits. When you go to the gym for the first time, you will find many sports equipment, one of them For slimming and burning calories, one of which is for the muscles, one for the joints and knees, and other different sports equipment. In order to be more experienced when choosing the appropriate sports equipment for your body or your goal,
+            learn with us about the 10 best types of sports equipment and their benefits.</h6>
+        <div class="features-list">
+            <div class="feature">
+                <div class="lists">
+                    <h2>1-Treadmill</h2>
+                    <div class="list">
+                        <h4>The average price ranges from 2,755 to 62,899 Egyptian pounds.</h4>
+                        <p>After the treadmill or treadmill, it is one of the best and most prominent sports equipment
+                            that occupies the forefront of our list today. The device burns approximately 100 calories per mile, and it
+                            is one of the types of devices that achieve the opportunity to reach the ideal weight in a very short time.
+                            It contains different speeds and levels, so you can practice walking, jogging, running or jogging with ease.
+                            Very suitable for beginners to exercise, easy to use and practice. 
+                            The electric treadmill is distinguished by its straightness and does not contain any curves or slopes that may cause
+                            any injury or sudden fall. Therefore, it is considered a completely safe electrical device.
+                            However, it is not recommended to use it for those who suffer from severe knee or joint pain, or who consult a specialist doctor
+                            about the condition. Walking many miles on a winding and curved road saves you, so you can walk or run for kilometers while you are in your place while burning very high calories.</p>
+                    </div>
+                </div>
+            </div>
+            <div class="feature">
+                <div class="lists">
+                    <h2>2-Stationary bike gym</h2>
+                    <div class="list">
+                        <h4>The average price ranges from 500 to 6,000 Egyptian pounds.</h4>
+                        <p>The stationary bike or stationary bike is among the very important equipment in sports salons.
+                        The treadmill helps burn high calories and reach the ideal weight in no time. Quick in case of alertness.
+                        The exercise also helps to strengthen the muscles of the joints and knees at an excellent rate.
+                        It does not cause any health harm to the knees, joints or vertebrae, but rather the opposite. It is known that riding a bike 
+                        and walking for many miles helps to lose weight, but it is considered hardship for some because of the slopes and uneven roads.
+                        But the bicycle device saves you hardship and fatigue, as it is stable, as if you are walking on the road completely, and the same 
+                        benefits and burning calories.One of the most important features of the fixed wheel device is that it works to adjust the blood pressure rate in the body,
+                        which helps prevent many diseases and enhances mental and physical health.
+                        </p>
+                    </div>
+                </div>
+            </div>
+            <div class="feature">
+                <div class="lists">
+                    <h2>3-Orbitrac</h2>
+                    <div class="list">
+                        <h4>The average price ranges from 3,000 to 18,000 Egyptian pounds.</h4>
+                        <p>The elliptical machine is one of the most important devices that we offer today in our list
+                             of the best 10 types of sports equipment.It gives excellent results in tightening the body’s strength and in a short
+                              period while regular exercise on it, helps to tighten sagging of the entire body such as the abdomen, thighs, arms, sides,
+                               helps to redistribute body fat, a device that is not arduous at all, that is, it can be practiced on it For an hour without 
+                               tiredness or exhaustion, it improves the psychological and mood of the player, suitable for the elderly, as it does not cause
+                                pain in the joints or knees, eliminating the fat of the buttocks, chest and waist, and tightening the buttocks and slimming the
+                                 chest.
+                        </p>
+                    </div>
+                </div>
+            </div>
+            
+            <div class="feature">
+                <div class="lists">
+                    <h2>4-Lat Pulldown Machine</h2>
+                    <div class="list">
+                        <h4>The average price ranges from 10,000 to 33,000 Egyptian pounds.</h4>
+                        <p>The pull-down machine is among the best equipment that is found in the gym and resistance exercises are
+                             practiced with it.
+                             This device targets the muscles of the back and the muscles of the forearms to make them stronger endurance.
+                             This device helps relieve back pain and protect and prevent sciatica, which causes pain starting from the
+                              back down to the feet. Helps to show the muscles of the back and forearms in a consistent manner.
+                        </p>
+                    </div>
+                </div>
+            </div>
+            
+            <div class="feature">
+                <div class="lists">
+                    <h2>5-Rowing Machine</h2>
+                    <div class="list">
+                        <h4>The average price ranges from 1,500 to 33,000 Egyptian pounds.</h4>
+                        <p>Rejuvenation is one of the best exercises that help shape muscles and improve the appearance of
+                             the chest, shoulders and back. Rowing also greatly helps to increase the burning rates in the body
+                             while giving the body energy, activity and vitality. In sports salons, there is a device similar to rowing
+                             in a lake with a small boat, but it is a fixed device. The device works to strengthen the main muscles,
+                             while making a big difference in the lower and upper part of the body together. Helps relieve back pain
+                             while strengthening muscles and vertebrae. Strongly strengthening the muscles of the chest and shoulders.
+                             It gives activity, energy, vitality, and the ability to carry heavy objects, as it helps to strengthen 
+                             the wrists. An easy-to-use device, but it is required to sit and practice in a way that your trainer
+                             determines for you.
+                        </p>
+                    </div>
+                </div>
+            </div>
+
+            <div class="feature">
+                <div class="lists">
+                    <h2>6-Chest fly machine</h2>
+                    <div class="list">
+                        <h4>The average price ranges from 7,000 Egyptian pounds.</h4>
+                        <p>The chest exerciser is one of the very important sports equipment in any sports salon, so the chest exercise is
+                             not complete without this device. The device is seated on the chair with the arms pulled, and it is possible
+                             to do more than one chest exercise on this device, and it is possible to load any weight in the device with 
+                             ease.The device helps to strengthen the pectoralis major muscles, the pectoralis minor muscles, and the anterior
+                             serratus muscles. It also helps to strengthen the biceps and triceps. After chest exercises on this device, you
+                             will notice swelling in the chest muscles immediately. Jhar is easy to use, but you must follow the correct way
+                             of sitting and pulling the weights the way the trainer explains to you.
+                        </p>
+                    </div>
+                </div>
+            </div>
+
+            <div class="feature">
+                <div class="lists">
+                    <h2>7-reverse hyper machine</h2>
+                    <div class="list">
+                        <h4>The average price ranges from 3,000 Egyptian pounds.</h4>
+                        <p>The reverse exercise machine is one of the most important health and beneficial sports equipment for the health of the
+                             vertebrae and back muscles. This device helps to stretch and straighten the spine in a healthy way.
+                             It is also considered one of the most important therapeutic methods that many people who suffer from back pain and problems 
+                             resort to. Lying on the stomach on the device, with the hands fully extended and holding the handles of the device. As for the feet,
+                             the weights are pulled straight, and the weights are pulled as desired.
+                        </p>
+                    </div>
+                </div>
+            </div>
+
+            <div class="feature">
+                <div class="lists">
+                    <h2>8-Leg Cart Machine</h2>
+                    <div class="list">
+                        <h4>The average price ranges from 10,000 Egyptian pounds.</h4>
+                        <p>Strengthening the thigh and leg muscles is one of the necessary things that any young man or woman and
+                             the elderly seek. The leg extension device helps to strengthen the muscles of the thigh and leg in 
+                             a healthy way, and if you continue to practice, the muscles in the thigh and leg are formed with a noticeable
+                             tightening in the back. It is profitable and not arduous in practice, very easy to use, healthy and does
+                             not cause any harm to the knee or joints.
+                        </p>
+                    </div>
+                </div>
+            </div>
+
+            <div class="feature">
+                <div class="lists">
+                    <h2>9-Abdominal Bench</h2>
+                    <div class="list">
+                        <h4>The average price ranges from 2,000 Egyptian pounds.</h4>
+                        <p>The abdominal exercise machine is one of the important and very useful sports equipment for the health
+                             of the body. The most areas of the body that cause distress and excretion, and many people want to lose
+                              weight and tighten them, is the abdominal area. The abdominal area is also one of the most areas in the
+                               body that does not lose weight except with diet and exercise to tighten the sagging abdomen and increase
+                                burning rates while strengthening the abdominal muscles. This device helps to practice all types and forms
+                                 of different abdominal exercises. Helps slim the lower abdomen and upper abdomen and get rid of fat
+                                  accumulated in the waist. It is easy to use and very profitable. It is much better than exercising 
+                                  while lying on the ground, as the device is padded and is better for the health of the joints.
+                        </p>
+                    </div>
+                </div>
+            </div>
+
+            <div class="feature">
+                <div class="lists">
+                    <h2>10-Balance Board</h2>
+                    <div class="list">
+                        <h4>The average price ranges from 1,500 Egyptian pounds.</h4>
+                        <p>Though this product might seem like not doing a lot, would you mind burning some calories 
+                            while working? This balance board for standing desks from Autonomous is everything you need 
+                            for those who find it hard to allocate separate time to work out. Made to encourage micro-movements,
+                             this flow or balance board burns calories and also improves heart health. It lowers the blood cholesterol
+                              level and regulates sugar after meals.
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+</div>
